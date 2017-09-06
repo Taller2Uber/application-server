c2e70fad691a52923158fa318ca789c73090cee6
agustinperrotta_31@hotmail.com <peragu>
2017-09-06 00:21:01 -0300

    mocking create user POST

--- llevame/__init__.py
+++ llevame/__init__.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 import flask_restful
 from flask_pymongo import PyMongo
 from flask import make_response
+
 
 MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
@@ -16,6 +17,19 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     return '<h1> All Working </h1>'
+
+
+@app.route("/api/user/create", methods=['POST'])
+def createUser():
+    user = {
+        'nombre': 'Agustin',
+        'apellido': 'Perrotta',
+        'email': 'aperrotta@gmail.com', 
+        'fbAccount': 'test',
+        'gmail' : 'suga92@gmail.com'
+    }
+
+    return jsonify(user), 200
 
 if __name__ == "__main__":
     app.run()
