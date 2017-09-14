d9b0b6ab651b04ab5edf5321b1e4bc61b801d1d3
fcaldora <facundo.caldora@gmail.com>
2017-09-13 22:37:03 -0300

    Agrego archivos de sphinx para documentar codigo

---
+++ llevame/docs/source/llevame.rst
@@ -0,0 +1,22 @@
+llevame package
+===============
+
+Submodules
+----------
+
+llevame\.llevame module
+-----------------------
+
+.. automodule:: llevame.llevame
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: llevame
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- llevame/llevame.py
+++ llevame/llevame.py
@@ -5,6 +5,39 @@ app = Flask(__name__)
 
 @app.route("/api/user/create", methods=['POST'])
 def createUser():
+    """Documentation example.
+
+    .. :quickref: Description.
+
+    **Example request**:
+
+    .. sourcecode:: http
+
+      POST / HTTP/1.1
+      Host: http://localhost:5000/api/user/create
+      Accept: application/json
+
+    **Example response**:
+
+    .. sourcecode:: http
+
+      HTTP/1.1 200 OK
+      Vary: Accept
+      Content-Type: application/json
+
+      {
+        'nombre': 'Agustin',
+        'apellido': 'Perrotta',
+        'email': 'aperrotta@gmail.com', 
+        'fbAccount': 'test',
+        'gmail' : 'suga92@gmail.com'
+      }
+
+    :query q: full text search query
+    :resheader Content-Type: application/json
+    :status 201: user created succesfully
+    :returns: :class:`myapp.objects.user`
+    """
     user = {
         'nombre': 'Agustin',
         'apellido': 'Perrotta',
@@ -13,7 +46,5 @@ def createUser():
         'gmail' : 'suga92@gmail.com'
     }
 
-    return jsonify(user), 200
+    return jsonify(user), 201
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
