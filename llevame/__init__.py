5b42c70445601a5cad63ab2e3523f9600d043d53
agustinperrotta_31@hotmail.com <peragu>
2017-10-08 20:14:33 -0300

    - driver creation - passenger creation - driver/passenger login #22

--- llevame/__init__.py
+++ llevame/__init__.py
@@ -34,18 +34,42 @@ user_token = api.model('User token', {
     'fb_token': fields.String(required=True, description='User\s facebook token')
 })
 
+user = api.model('User', {
+    'fb_id': fields.String(),
+    'fb_token': fields.String(required=True, description='User\s facebook token'),
+    'name': fields.String(),
+    'gender': fields.String(),
+    'email': fields.String()
+})
+
 card = api.model('Card', {
     'number': fields.String(required=True),
     'expiration_date': fields.DateTime(required=True),
     'company': fields.String(required=True)
 })
 
-user = api.model('User', {
-    'fb_token': fields.String(required=True, description='User\s facebook token'),
-    'latitude': fields.Integer(required=True, default=0),
-    'longitude': fields.Integer(required=True, default=0),
-    'card': fields.Nested(card)
+car = api.model('Car', {
+    'model': fields.String(required=True),
+    'brand': fields.String(required=True),
+    'year': fields.Integer(required=True),
+    'license_plate': fields.String(required=True),
+    'ac': fields.Boolean(required=True)})
+
+driver = api.model('Driver', {
+    'latitude': fields.Integer(),
+    'longitude': fields.Integer(),
+    'cars': fields.List(fields.Nested(car),required=True),
+    'user': fields.Nested(user)
 })
+
+passenger = api.model('passenger', {
+    'latitude': fields.Integer(),
+    'longitude': fields.Integer(),
+    'card': fields.Nested(card, required=True),
+    'user': fields.Nested(user)
+})
+
+
 
 
 @api.route('/api/v1/users')
@@ -59,24 +83,86 @@ class UserController(Resource):
     def post(self):
         # Me quedo con el token
         fb_token = request.json['fb_token']
-        if not fb_token:
-            return 'Token not found', 400
         # Request a facebook
-        #fb_response = requests.get(
-         #   'https://graph.facebook.com/me?access_token=' + fb_token + '&fields=name,gender,email').content
-        #fb_body = json.loads(fb_response)
-        fb_body = {'id': 15}
+        fb_response = requests.get('https://graph.facebook.com/me?access_token=' + fb_token + '&fields=name,gender').content
+        fb_body = json.loads(fb_response)
         if 'error' not in fb_body:
             users = mongo.db.users
-            user = users.find_one({'user_id': fb_body['id']})
+            user = users.find_one({'fb_id': fb_body['id']})
             if not user:
-                users.insert(request.json)
-                return json.loads(dumps(request.json)), 201, {'Content-type': 'application/json'}
+                user_to_insert = {'fb_id': fb_body['id'],
+                    'fb_token': fb_token,
+                    'name': fb_body.get('name'),
+                    'gender': fb_body.get('gender')}
+                users.insert(user_to_insert)
+                return json.loads(dumps(user_to_insert)), 201, {'Content-type': 'application/json'}
             else:
-                return 'User already registered', 400
+                return {'error': 'User already registered'}, 400, {'Content-type': 'application/json'}
         # Devuelvo el error de fb.
         return fb_body, 400
 
+@api.route('/api/v1/drivers')
+class DriversController(Resource):
+    @api.response(200, 'Success')
+    def get(self):
+        db_drivers = dumps(mongo.db.drivers.find())
+        return json.loads(db_drivers), 200, {'Content-type': 'application/json'}
+
+    @api.expect(driver, validate=True)
+    def post(self):
+        # Me quedo con el token
+        fb_token = request.json['fb_token']
+        # Request a facebook
+        fb_response = requests.get('https://graph.facebook.com/me?access_token=' + fb_token + '&fields=name,gender').content
+        fb_body = json.loads(fb_response)
+        if 'error' not in fb_body:
+            drivers = mongo.db.drivers
+            driver = drivers.find_one({'user.fb_id': fb_body['id']})
+            if not driver:
+                driver_to_insert = {'user': {'fb_id':fb_body['id'], 'fb_token': fb_token,
+                    'name': fb_body.get('name'),
+                    'gender': fb_body.get('gender')},
+                    'latitude': request.json.get('latitude'),
+                    'longitude': request.json.get('longitude'),
+                    'cars': request.json.get('cars')}
+                drivers.insert(driver_to_insert)
+                return json.loads(dumps(driver_to_insert)), 201, {'Content-type': 'application/json'}
+            else:
+                return {'error': 'Driver already registered'}, 400, {'Content-type': 'application/json'}
+        # Devuelvo el error de fb.
+        return fb_body, 400
+
+@api.route('/api/v1/passengers')
+class PassengersController(Resource):
+    @api.response(200, 'Success')
+    def get(self):
+        db_passengers = dumps(mongo.db.passengers.find())
+        return json.loads(db_passengers), 200, {'Content-type': 'application/json'}
+
+    @api.expect(passenger, validate=True)
+    def post(self):
+        # Me quedo con el token
+        fb_token = request.json['fb_token']
+        # Request a facebook
+        fb_response = requests.get(
+            'https://graph.facebook.com/me?access_token=' + fb_token + '&fields=name,gender').content
+        fb_body = json.loads(fb_response)
+        if 'error' not in fb_body:
+            passengers = mongo.db.passengers
+            passenger = passengers.find_one({'user.fb_id': fb_body['id']})
+            if not passenger:
+                passenger_to_insert = {'user': {'fb_id': fb_body['id'], 'fb_token': fb_token,
+                                             'name': fb_body.get('name'),
+                                             'gender': fb_body.get('gender')},
+                                    'latitude': request.json.get('latitude'),
+                                    'longitude': request.json.get('longitude'),
+                                    'card': request.json.get('card')}
+                passengers.insert(passenger_to_insert)
+                return json.loads(dumps(passenger_to_insert)), 201, {'Content-type': 'application/json'}
+            else:
+                return {'error': 'Passenger already registered'}, 400, {'Content-type': 'application/json'}
+        # Devuelvo el error de fb.
+        return fb_body, 400
 
 @api.route("/api/v1/users/login")
 class UserLoginController(Resource):
@@ -90,17 +176,17 @@ class UserLoginController(Resource):
         fb_response = requests.get('https://graph.facebook.com/me?access_token=' + fb_token).content
         fb_body = json.loads(fb_response)
         if 'error' not in fb_body:
-            users = mongo.db.users
-            user = users.find_one({'user_id': fb_body['id']})
-            if not user:
+            passengers = mongo.db.passengers
+            passenger = passengers.find_one({'user.fb_id': fb_body['id']})
+            if not passenger:
                 drivers = mongo.db.drivers
-                driver = drivers.find_one({'user_id': fb_body['id']})
+                driver = drivers.find_one({'user.fb_id': fb_body['id']})
                 if not driver:
                     return 'User not registered', 400
                 else:
-                    return JSONEncoder().encode(driver), 200, {'Content-type': 'application/json'}
+                    return json.loads(dumps(driver)), 200, {'Content-type': 'application/json'}
             else:
-                return JSONEncoder().encode(user), 200, {'Content-type': 'application/json'}
+                return json.loads(dumps(passenger)), 200, {'Content-type': 'application/json'}
         # Devuelvo user.
         return fb_response, 400
 
