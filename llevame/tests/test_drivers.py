a2ba231d12a6e2d0b8400960aacb801934add032
fcaldora <facundo.caldora@gmail.com>
2017-11-28 22:54:09 -0300

    Agrego test #56

---
+++ llevame/tests/test_drivers.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+import flask
+import llevame
+
+class AuthTestCase(unittest.TestCase):
+    """Test case for the authentication blueprint."""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.app = llevame.app.test_client()
+        self.app.testing = True
+        self.app.config.update(
+            MONGO_URI="mongodb://root:qmsroot@ds147681.mlab.com:47681/llevame_test"
+        )
+
+    def test_registration(self):
+        """Test user registration works correcty."""
+        res = self.app.get('/api/v1/drivers')
+        result = json.loads(res.data.decode())
+        self.assertEqual(
+            result['message'], "You registered successfully. Please login.")
+        self.assertEqual(res.status_code, 200)
